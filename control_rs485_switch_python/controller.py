7b6c02bef6343f491d31c0f7a9265c3329d37fe3
shuijing1024 <shuijing1024@outlook.com>
2024-12-22 12:45:40 +0800

    简化了控制器内部操作，提高了性能。与之对应的是python只能在一个线程中使用此控制器实例。

--- control_rs485_switch_python/controller.py
+++ control_rs485_switch_python/controller.py
@@ -5,38 +5,78 @@ from .control_rs485_switch_python import PythonSwitchController,PythonSwitchStat
 
 @dataclass(frozen=True)
 class USBSerialPortInfo:
-    port_name:str
-    port_label:str
+    """
+    USB串口信息。
+    """
+
+    port_name:str   # 串口名
+    port_label:str  # 串口标签，例如Windows上会显示CH340。如果没有的话就和串口名一致。
 
 @dataclass(frozen=True)
 class ModbusConfig:
-    port_name:str
-    baud_rate:int=4800
-    slave_id:int=1
-    timeout:int=6000
+    """
+    Modbus配置
+    """
+
+    port_name:str   #串口名
+    baud_rate:int=4800  # 波特率
+    slave_id:int=1  # 从机设备ID
+    timeout:int=6000    # Modbus操作超时时间，单位为毫秒
 
 class SwitchState(Enum):
-    Close=0
-    Open=1
-    Lock=2
-    Unlock=3
+    """
+    开关状态
+    """
+
+    Close=0 # 分闸
+    Open=1  # 合闸
+    Lock=2  # 锁定
+    Unlock=3    # 解锁。在读取开关情况下，不会返回此状态。
 
 
 class SwitchController:
-    def __init__(self,config:ModbusConfig):
+    """
+    这是控制开关操作的Python类。\n
+    为了简化操作，请不要将该控制类实例于多线程中使用，可能会出现未知情况。\n
+    当建立类实例时即为连接上通信串口。\n
+    类中所有方法都有可能返回RuntimeError，其中参数是字符串，描述了错误原因。
+    """
+
+    def __init__(self, config: ModbusConfig):
+        """
+        初始化。会打开通信串口。
+        :param config: Modbus配置
+        """
+
         self.__controller=None
         self.__controller=PythonSwitchController(config)
 
     @staticmethod
     def get_usb_serial_port_list()->list[USBSerialPortInfo]:
+        """
+        获得USB串口列表
+        :return: USB串口列表
+        """
+
         serialList=PythonSwitchController.get_usb_serial_port_list()
         return [USBSerialPortInfo(port_name=serial.port_name,port_label=serial.port_label) for serial in serialList]
 
     def custom_init(self,config:ModbusConfig)->int:
+        """
+        初始化。\n
+        假如开关不是Modbus协议，切换为Modbus协议，并返回Modbus协议下的设备ID
+        :return: 设备ID
+        """
+
         return self.__controller.custom_init(config)
 
     def get_switch_state(self)->SwitchState:
-        switch_state=self.__controller.get_switch_state()
+        """
+        获得开关状态
+        :return: 开关状态
+        """
+
+        switch_state= self.__controller.get_switch_state
         if switch_state==PythonSwitchState.Close:
             return SwitchState.Close
         elif switch_state==PythonSwitchState.Open:
@@ -48,18 +88,35 @@ class SwitchController:
         else:
             raise RuntimeError("未知开关状态")
 
-    def open_switch(self):
+    def open_switch(self)->None:
+        """
+        合闸
+        """
+
         switch_state=PythonSwitchState.Open
         self.__controller.operate_switch(switch_state)
 
-    def close_switch(self):
+    def close_switch(self)->None:
+        """
+        分闸
+        """
+
         switch_state=PythonSwitchState.Close
         self.__controller.operate_switch(switch_state)
 
-    def set_baud_rate(self, baud_rate:int):
+    def set_baud_rate(self, baud_rate:int)->None:
+        """
+        设置波特率。就目前来看，波特率最高能有115200，最低4800。刚到手时默认115200。
+        :param baud_rate: 要设置的波特率
+        """
+
         self.__controller.set_baud_rate(baud_rate)
 
 
     def __del__(self):
+        """
+        在此控制器删除时进行一些清理操作
+        """
+
         if self.__controller is not None:
             self.__controller.disconnect()
--- src/backend/operate.rs
+++ src/backend/operate.rs
@@ -3,28 +3,24 @@ use super::utils::*;
 
 use lib_switch_operate::prelude::SwitchController as RustSwitchController;
 
-use std::sync::{Arc, Mutex};
-
 use pyo3::exceptions::PyRuntimeError;
 use pyo3::prelude::*;
 use tokio::runtime::Runtime;
 
 #[pyclass]
 pub(crate) struct PythonSwitchController {
-    runtime: Arc<Runtime>,
-    controller: Arc<Mutex<RustSwitchController>>,
+    runtime: Runtime,
+    controller: RustSwitchController,
 }
+
+unsafe impl Sync for PythonSwitchController {}
 
 #[pymethods]
 impl PythonSwitchController {
     #[new]
     fn new(config: PythonModbusConfig) -> PyResult<Self> {
         let runtime = Runtime::new().map_err(|_e| PyRuntimeError::new_err("创建运行环境失败"))?;
-
         let controller = RustSwitchController::new(config.into()).map_to_py_result()?;
-
-        let runtime = Arc::new(runtime);
-        let controller = Arc::new(Mutex::new(controller));
 
         Ok(Self {
             runtime,
@@ -49,9 +45,8 @@ impl PythonSwitchController {
         self.runtime.block_on(action).map_to_py_result()
     }
 
-    fn get_switch_state(&self) -> PyResult<PythonSwitchState> {
-        let mut controller = self.controller.lock().map_to_py_result()?;
-        let action = controller.get_switch_state();
+    fn get_switch_state(&mut self) -> PyResult<PythonSwitchState> {
+        let action = self.controller.get_switch_state();
 
         let state = self.runtime.block_on(action).map_to_py_result()?;
 
@@ -60,22 +55,19 @@ impl PythonSwitchController {
 
     fn operate_switch(&mut self, operation_state: &PythonSwitchState) -> PyResult<()> {
         let custom_state: RustSwitchState = RustSwitchState::from(operation_state.clone());
-        let mut controller = self.controller.lock().map_to_py_result()?;
-        let action = controller.operate_switch(custom_state);
+        let action = self.controller.operate_switch(custom_state);
 
         self.runtime.block_on(action).map_to_py_result()
     }
 
     fn set_baud_rate(&mut self, baud_rate: u32) -> PyResult<()> {
-        let mut controller = self.controller.lock().map_to_py_result()?;
-        let action = controller.set_baud_rate(baud_rate);
+        let action = self.controller.set_baud_rate(baud_rate);
 
         self.runtime.block_on(action).map_to_py_result()
     }
 
     fn disconnect(&mut self) -> PyResult<()> {
-        let mut controller = self.controller.lock().map_to_py_result()?;
-        let action = controller.disconnect();
+        let action = self.controller.disconnect();
 
         self.runtime.block_on(action).map_to_py_result()
     }
